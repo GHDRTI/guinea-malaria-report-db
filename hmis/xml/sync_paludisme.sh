d57b372daf8a0f1f4b243301b2431211902076fb
Adam Preston <adam.m.preston@gmail.com>
2019-05-09 13:35:57 -0600

    use environment variables instead of STDIN

--- hmis/xml/sync_paludisme.sh
+++ hmis/xml/sync_paludisme.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
+# be sure to set the environement vars
+#
+# GUINEA_HMIS_USER
+# GUINEA_HMIS_PASS
+# DHIS2_USER
+# DHIS2_PASS
 
 echo ""
 echo ""
 echo "This script will download malaria data from the national HMIS and transform it, then upload it to the stop Palu database."
 echo ""
 
-echo "Enter the National HMIS username:"
 
-read moh_user
-
-echo "Enter the National HMIS password:"
-
-read moh_pass
 
 echo "Enter the period (i.e. 201805 for May 2018)"
 
 read period
 
 # get moh data
-curl -u $moh_user:$moh_pass "https://dhis2.sante.gov.gn/dhis/api/26/dataValueSets.xml?dataSet=TL7xWT7zMNN&period="${period}"&orgUnitGroup=UF9kVV05HJ0" > paludisme_moh_${period}.xml
+curl -u $GUINEA_HMIS_USER:$GUINEA_HMIS_PASS "https://dhis2.sante.gov.gn/dhis/api/26/dataValueSets.xml?dataSet=TL7xWT7zMNN&period="${period}"&orgUnitGroup=UF9kVV05HJ0" > paludisme_moh_${period}.xml
 
 
 # you can get this by doing this on a mac 'brew install saxon'
 saxon -xsl:paludisme_transform.xsl -s:paludisme_moh_${period}.xml -o:paludisme_stoppalu_${period}.xml
 
 
-echo "Enter your Stop Palu username:"
-
-read stoppalu_user
-
-echo "Enter your Stop Palu password:"
-
-read stoppalu_pass
-
-
-curl -u $stoppalu_user:$stoppalu_pass -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/26/dataValueSets?importStrategy=CREATE" --data @paludisme_stoppalu_${period}.xml
+curl -u $DHIS2_USER:$DHIS2_PASS -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/26/dataValueSets?importStrategy=CREATE" --data @paludisme_stoppalu_${period}.xml
 
 
 saxon -xsl:paludisme_complete_registrations.xsl -s:paludisme_moh_${period}.xml -o:paludisme_stoppalu_${period}_completes.xml
