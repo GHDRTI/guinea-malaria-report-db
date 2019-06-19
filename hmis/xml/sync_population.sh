66155bc9a45b026e288facc1e240906cc259214a
Adam Preston <adam.m.preston@gmail.com>
2019-06-19 12:32:38 +0200

    updated call to api for 2.32

--- hmis/xml/sync_population.sh
+++ hmis/xml/sync_population.sh
@@ -1,47 +1,42 @@
 #!/bin/bash
+# be sure to set the environement vars
+#
+# GUINEA_HMIS_USER
+# GUINEA_HMIS_PASS
+# DHIS2_USER
+# DHIS2_PASS
 
 echo ""
 echo ""
-echo "This script will download population data from the national HMIS and transform it, then upload it to the stop Palu database."
+echo "This script will download malaria data from the national HMIS and transform it, then upload it to the stop Palu database."
 echo ""
 
-echo "Enter the National HMIS username:"
 
-read moh_user
 
-echo "Enter the National HMIS password:"
-
-read moh_pass
-
-echo "Enter the period (i.e. 2018 or 2017)"
+echo "Enter the period (i.e. 201805 for May 2018)"
 
 read period
 
-# qhAGuWjFjFG is the MOH population dataset
-curl -u $moh_user:$moh_pass "https://dhis2.sante.gov.gn/dhis/api/26/dataValueSets.xml?dataSet=qhAGuWjFjFG&period="$period"&orgUnitGroup=UF9kVV05HJ0" > population_moh_$period.xml
+# get moh data
+curl -u $GUINEA_HMIS_USER:$GUINEA_HMIS_PASS "https://dhis2.sante.gov.gn/dhis/api/26/dataValueSets.xml?dataSet=TL7xWT7zMNN&period="${period}"&orgUnitGroup=UF9kVV05HJ0" > paludisme_moh_${period}.xml
+
 
 # you can get this by doing this on a mac 'brew install saxon'
-saxon -xsl:population_transform.xsl -s:population_moh_$period.xml -o:population_stoppalu_$period.xml
-
-echo "Enter your Stop Palu username:"
-
-read stoppalu_user
-
-echo "Enter your Stop Palu password:"
-
-read stoppalu_pass
+saxon -xsl:paludisme_transform.xsl -s:paludisme_moh_${period}.xml -o:paludisme_stoppalu_${period}.xml
 
 
-curl -u $stoppalu_user:$stoppalu_pass -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/26/dataValueSets" --data @population_stoppalu_$period.xml
+curl -u $DHIS2_USER:$DHIS2_PASS -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/dataValueSets.xml?importStrategy=CREATE" --data @paludisme_stoppalu_${period}.xml
 
-# complete these datasets
-saxon -xsl:population_complete_registrations.xsl -s:population_moh_$period.xml -o:population_stoppalu_$period_completes.xml
+
+
+
+saxon -xsl:paludisme_complete_registrations.xsl -s:paludisme_moh_${period}.xml -o:paludisme_stoppalu_${period}_completes.xml
 
 echo ""
 echo "Completing Datasets."
 echo ""
 
-curl -u $stoppalu_user:$stoppalu_pass -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/26/completeDataSetRegistrations.xml" --data @population_stoppalu_$period_completes.xml
+curl -u $DHIS2_USER:$DHIS2_PASS -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/completeDataSetRegistrations.xml" --data @paludisme_stoppalu_${period}_completes.xml
 
 
 echo ""
@@ -50,5 +45,6 @@ echo ""
 
 echo "Ok, you're done. Regenerate the analytic tables and you should be good (unless of course you have errors."
 
-echo ""
 
+echo ""
+echo ""
