c145c744317320e2bc0520f26acfa58713de7dd3
Adam Preston <adam.m.preston@gmail.com>
2019-09-10 13:43:03 -0600

    fixed population script to use right variables

--- hmis/xml/sync_population.sh
+++ hmis/xml/sync_population.sh
@@ -8,7 +8,7 @@
 
 echo ""
 echo ""
-echo "This script will download malaria data from the national HMIS and transform it, then upload it to the stop Palu database."
+echo "This script will download population data from the national HMIS and transform it, then upload it to the stop Palu database."
 echo ""
 
 
@@ -18,25 +18,25 @@ echo "Enter the period (i.e. 201805 for May 2018)"
 read period
 
 # get moh data
-curl -u $GUINEA_HMIS_USER:$GUINEA_HMIS_PASS "https://dhis2.sante.gov.gn/dhis/api/26/dataValueSets.xml?dataSet=TL7xWT7zMNN&period="${period}"&orgUnitGroup=UF9kVV05HJ0" > paludisme_moh_${period}.xml
+curl -u $GUINEA_HMIS_USER:$GUINEA_HMIS_PASS "https://dhis2.sante.gov.gn/dhis/api/26/dataValueSets.xml?dataSet=TL7xWT7zMNN&period="${period}"&orgUnitGroup=UF9kVV05HJ0" > population_moh_${period}.xml
 
 
 # you can get this by doing this on a mac 'brew install saxon'
-saxon -xsl:paludisme_transform.xsl -s:paludisme_moh_${period}.xml -o:paludisme_stoppalu_${period}.xml
+saxon -xsl:population_transform.xsl -s:population_moh_${period}.xml -o:population_stoppalu_${period}.xml
 
 
-curl -u $DHIS2_USER:$DHIS2_PASS -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/dataValueSets.xml?importStrategy=CREATE" --data @paludisme_stoppalu_${period}.xml
+curl -u $DHIS2_USER:$DHIS2_PASS -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/dataValueSets.xml?importStrategy=CREATE" --data @population_stoppalu_${period}.xml
 
 
 
 
-saxon -xsl:paludisme_complete_registrations.xsl -s:paludisme_moh_${period}.xml -o:paludisme_stoppalu_${period}_completes.xml
+#saxon -xsl:paludisme_complete_registrations.xsl -s:population_moh_${period}.xml -o:population_stoppalu_${period}_completes.xml
 
 echo ""
 echo "Completing Datasets."
 echo ""
 
-curl -u $DHIS2_USER:$DHIS2_PASS -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/completeDataSetRegistrations.xml" --data @paludisme_stoppalu_${period}_completes.xml
+#curl -u $DHIS2_USER:$DHIS2_PASS -H "Content-Type: application/xml"  "https://stoppaludhis2.rti-ghd.org/api/completeDataSetRegistrations.xml" --data @paludisme_stoppalu_${period}_completes.xml
 
 
 echo ""
